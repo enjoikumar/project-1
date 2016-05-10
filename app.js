54ce7d852b90d9c46cd0190c694e6e6adc707b08
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2016-05-10 11:45:17 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("ice cream")
+
+//	Project Plan
+// PsuedoCode it all out on Tuesday which includes the logic, and knowing what to do. 
+//Breaking down every aspect into doable steps
+
+// Tuesday:PsuedoCoding and basic css styling
+// Wednesday: Get the java down step-by-step
+// the beginning: player inputs
+// middle: making the columns correspond to clicks
+// end: check for win and a reset button
+// Thursday: Css styling and line by line reading finding any type of bug
+// YOU'RE NEVER GONNA BE AHEAD OF SCHEDULE, IF YOU'RE DONE
+// WORK ON THE NEXT ASPECT
+// Maybe a user story to better depict what will go on to better understand the logic
+// Wire framing for layout purposes
+// Jquery option?
+
+//Requirements
+// Two different colors for players (red & black)
+// A 2 player game
+// The game board should have 7 columns and 6 rows
+// Simulating gravity / Only when a row is clicked should a piece move all the way down the column to the next available spot
+// So many different winning combinations
+// Need to keep in mind of diagonals and such
+// That will go in for check/win
+// Reset button(window.reload)
+// Declare winner to go with the winning combination
+// Alert 'name' won at the end or alert 'it’s a tie'
+// Require set name
+// Make player1/2 an object=name:''/color:'red/black'
+// CSS should be taken in consideration last
+// But I need to be able to have rows and columns with the grid have circles in it
+// Or making a game with boxes, much easier if you cant get the circular tiles to work
+// I want the name input to say player1 vs player2 (kk vs cc)
+// There are only 42 moves, and each player has 21 moves
